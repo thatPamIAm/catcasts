f8391f984f6c77d6217a6d1ae05f2e0528a048ec
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-12-06 21:19:33 -0700

    Updte styling and main message

--- lib/catcasts_web/controllers/page_controller.ex
+++ lib/catcasts_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule CatcastsWeb.PageController do
   use CatcastsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_flash(:info, "This is a test")
+    |> render("index.html")
   end
 end
